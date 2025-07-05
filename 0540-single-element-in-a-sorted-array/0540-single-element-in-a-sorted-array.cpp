422a78f9baed304c7c6c400c0c6bfefa5710dd96
shashiranjan567 <121805814+shashiranjan567@users.noreply.github.com>
2025-07-05 14:39:20 +0530

    Time: 20 ms (3.86%), Space: 31.4 MB (7.29%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map < int , int > freq;
+        for(int i = 0; i < nums.size() ; i++){
+             freq[nums[i]]++;
+        }
+
+        for(int i = 0; i < nums.size() ; i++){
+             if(freq[nums[i]] == 1){
+                return nums[i];
+             }
+
+        }
+       
+    
+
+        return -1;
+        
+    }
+};
