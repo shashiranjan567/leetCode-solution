794d52b4d4af6f1d5a2e0e10ccfdb2bc84456784
shashiranjan567 <121805814+shashiranjan567@users.noreply.github.com>
2025-08-30 22:07:57 +0530

    Time: 0 ms (100%), Space: 15 MB (24.88%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,7 +8,80 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//FROM MYSELF
+//Approach-1 (2 Pass)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
+public:
+    
+    int lengthLinkedList(ListNode* head) {
+        int l = 0;
+        
+        while(head) {
+            l++;
+            head = head->next;
+        }
+        return l;
+    }
+    
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int length = lengthLinkedList(head);
+        
+        if(length == n) { //Length node form end means first node
+            ListNode* temp = head->next;
+            delete(head);
+            return temp;
+        }
+
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        int travel = length-n;
+        
+        while(travel--) {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        prev->next = temp->next;
+        
+        delete(temp);   
+        
+        return head;
+        
+    }
+};
+
+/*//Approach-2 (1 Pass)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        for(int i = 1; i <= n; i++) {
+            fast = fast->next;
+        }
+        if(fast == NULL) {
+            return head->next;
+        }
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete(temp);
+        return head;
+    }
+};*/
+
+/*class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         // Create a dummy node before head to handle edge cases
@@ -36,4 +109,4 @@ public:
 
         return dummy->next;
     }
-};
+};*/
