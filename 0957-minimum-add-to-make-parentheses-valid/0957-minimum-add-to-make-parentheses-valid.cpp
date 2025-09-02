e19c4abf7ddeac00b2078e1d08f8379b2531092b
shashiranjan567 <121805814+shashiranjan567@users.noreply.github.com>
2025-09-02 11:03:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (81%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,47 @@
+//Approach-1 (Using stack)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        int open = 0;
+        
+        for(char &ch : s) {
+            if(ch == '(')
+                st.push(ch);
+            else if(!st.empty())
+                st.pop();
+            else
+                open++;
+        }
+        
+        return open + st.size();
+    }
+};
+
+/*
+//Approach-2 (without using stack)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int size = 0;
+        int open = 0;
+
+        for(char &ch : s) {
+            if(ch == '(') {
+                size++;
+            } else if(size > 0) {
+                size--;
+            } else {
+                open++;
+            }
+        }
+
+        return open + size;
+    }
+};
+*/
+
