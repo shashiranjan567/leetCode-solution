e371038a746a81f02c32a3cdd8356dd8ae18afe5
shashiranjan567 <121805814+shashiranjan567@users.noreply.github.com>
2025-09-07 08:39:47 +0530

    Time: 3 ms (18.02%), Space: 23.1 MB (15.42%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//Approach-1 (Using 2 Pass Solution and LCA code)
+//T.C : O(2*n) ~= O(n)
+//S.C : O(maxDepth) System stack space
+class Solution {
+public:
+    unordered_map<int, int> mp;
+    int maxD = 0;
+
+    TreeNode* LCA(TreeNode* root) {
+        if(root == NULL || mp[root->val] == maxD) {
+            return root;
+        }
+
+        TreeNode* l = LCA(root->left);
+        TreeNode* r = LCA(root->right);
+
+        if(l && r) {
+            return root;
+        }
+
+        return l != NULL ? l : r;
+    }
+
+    void depth(TreeNode* root, int d) {
+        if(!root) {
+            return;
+        }
+
+        maxD = max(maxD, d);
+        mp[root->val] = d;
+        depth(root->left, d+1);
+        depth(root->right, d+1);
+    }
+    //T.C : O(n)
+    //S.C : O(maxD) system recursion stack space
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        depth(root, 0);
+
+        return LCA(root);
+    }
+};
+
+/*
+//Approach-2 (Using 1 Pass Solution)
+//T.C : O(n)
+//S.C : O(maxDepth) System stack space
+class Solution {
+public:
+    pair<int, TreeNode*> solve(TreeNode* root) {
+        if(root == NULL) {
+            return {0, NULL};
+        }
+
+        auto l = solve(root->left);
+        auto r = solve(root->right);
+
+        if(l.first == r.first) {
+            return {l.first+1, root};
+        } else if(l.first > r.first) {
+            return {l.first+1, l.second};
+        } else {
+            return {r.first+1, r.second};
+        }
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return solve(root).second;   
+    }
+};
+
+*/
