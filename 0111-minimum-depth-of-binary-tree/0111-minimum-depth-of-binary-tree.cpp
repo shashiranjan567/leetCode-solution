f477b1286b59be7a9a046e04a22b9df7fcc1969f
shashiranjan567 <121805814+shashiranjan567@users.noreply.github.com>
2025-09-07 08:52:16 +0530

    Time: 3 ms (53.36%), Space: 146.6 MB (88.64%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//Using Simple DFS - O(n)
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        if(root->left == NULL && root->right == NULL)
+            return 1;
+        
+        int left  = root->left?minDepth(root->left):INT_MAX;
+        int right = root->right?minDepth(root->right):INT_MAX;
+        
+        return 1 + min(left, right);
+    }
+};
+
+/*
+//Using Simple BFS - O(n)
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        if(!root->left && !root->right)
+            return 1;
+        
+        queue<TreeNode*> que;
+        que.push(root);
+        
+        int depth = 1;
+        
+        while(!que.empty()) {
+            
+            int n = que.size();
+            
+            while(n--) {
+                TreeNode* temp = que.front();
+                que.pop();
+                
+                if(!temp->left && !temp->right)
+                    return depth;
+                
+                if(temp->left)
+                    que.push(temp->left);
+                if(temp->right)
+                    que.push(temp->right);
+            }
+            
+            depth++;
+            
+        }
+        
+        return -1;
+    }
+};
+*/
